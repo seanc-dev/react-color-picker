1adb0036af45006a1c4d2968d0efdcbc699c5352
Sean Coley <seanwcoley@gmail.com>
2020-05-16 17:06:36 +0200

    Add snackbar and style select

--- src/Scenes/Palette/components/Navbar.component.js
+++ src/Scenes/Palette/components/Navbar.component.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Slider from "rc-slider";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
+import Snackbar from "@material-ui/core/Snackbar";
+import CloseIcon from "@material-ui/icons/Close";
+import IconButton from "@material-ui/core/IconButton";
 
 import "rc-slider/assets/index.css";
 import "./Navbar.component.css";
 
 function Navbar({ level, setLevel, format, setFormat }) {
-  const handleChange = (e) => setFormat(e.target.value);
+  const [open, setOpen] = useState(false);
+  const handleChange = (e) => {
+    setFormat(e.target.value);
+    setOpen(true);
+    setTimeout(() => setOpen(false), 3000);
+  };
+  const closeSnackbar = () => setOpen(false);
   return (
     <header className="Navbar">
       <div className="logo">
@@ -32,6 +41,27 @@ function Navbar({ level, setLevel, format, setFormat }) {
           <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
         </Select>
       </div>
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        open={open}
+        autoHideDuration={3000}
+        message={
+          <span id="snackbar">Format changed to {format.toUpperCase()}!</span>
+        }
+        ContentProps={{
+          "aria-describedby": "snackbar",
+        }}
+        action={[
+          <IconButton
+            onClick={closeSnackbar}
+            color="inherit"
+            key="close"
+            aria-label="close"
+          >
+            <CloseIcon />
+          </IconButton>,
+        ]}
+      />
     </header>
   );
 }
